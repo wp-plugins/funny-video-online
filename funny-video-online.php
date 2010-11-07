f75031ac53da631ebe84134311d06aa795cba97a
allis741 <allis741@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-11-07 13:27:57 +0000

    Adding first version of my plugin
    git-svn-id: https://plugins.svn.wordpress.org/funny-video-online/trunk@308758 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ funny-video-online.php
@@ -0,0 +1,153 @@
+<?php
+/*
+Plugin Name: Funny video online
+Plugin URI: http://www.onlinerel.com/wordpress-plugins/
+Description: Plugin "Funny video online" displays Funny video on your blog. There are over 10,000 video clips.
+Add Funny YouTube videos to your sidebar on your blog using  a widget.
+Version: 1.0
+Author: A.Kilius
+Author URI: http://www.onlinerel.com/wordpress-plugins/
+License: GPL2
+*/
+
+define(funny_video_online_READER_URL_RSS_DEFAULT, 'http://fun.onlinerel.com/category/funny-video/feed/');
+define(funny_video_online_READER_TITLE, 'Funny video online');
+define(funny_video_online_MAX_SHOWN_ITEMS, 3);
+
+
+function funny_video_online_widget_ShowRss($args)
+{
+	//@ini_set('allow_url_fopen', 1);	
+	if( file_exists( ABSPATH . WPINC . '/rss.php') ) {
+		require_once(ABSPATH . WPINC . '/rss.php');		
+	} else {
+		require_once(ABSPATH . WPINC . '/rss-functions.php');
+	}
+	
+	$options = get_option('funny_video_online_widget');
+
+	if( $options == false ) {
+		$options[ 'funny_video_online_widget_url_title' ] = funny_video_online_READER_TITLE;
+		$options[ 'funny_video_online_widget_RSS_url' ] = funny_video_online_READER_URL_RSS_DEFAULT;
+		$options[ 'funny_video_online_widget_RSS_count_items' ] = funny_video_online_MAX_SHOWN_ITEMS;
+	}
+
+ $RSSurl = funny_video_online_READER_URL_RSS_DEFAULT;
+	$messages = fetch_rss($RSSurl);
+	$title = $options[ 'funny_video_online_widget_url_title' ];
+	
+	$messages_count = count($messages->items);
+	if($messages_count != 0){
+		$output = '<ul>';	
+	for($i=0; $i<$options['funny_video_online_widget_RSS_count_items'] && $i<$messages_count; $i++)
+		{		
+	 $fotocode = explode("http://www.youtube.com/v/", $messages->items[$i]['content']['encoded']);
+     $fotocode = explode("?fs=1", $fotocode[1]);
+		$foto = '<img src="http://i2.ytimg.com/vi/'.$fotocode[0].'/default.jpg" width="150" border="0"   title="'.$messages->items[$i]['description'].'"/>';
+ $output .= '<li>';
+ $output .= '<a target ="_blank" href="'.$messages->items[$i]['link'].'">'.$foto.'</a></span>';	
+	 $output .= '</li>';
+	}
+		$output .= '</ul> ';
+	}
+	
+	extract($args);	
+	?>
+	<?php echo $before_widget; ?>
+	<?php echo $before_title . $title . $after_title; ?>	
+	<?php echo $output; ?>
+	<?php echo $after_widget; ?>
+	<?php	
+}
+
+function funny_video_online_widget_Admin()
+{
+	$options = $newoptions = get_option('funny_video_online_widget');	
+	//default settings
+	if( $options == false ) {
+		$newoptions[ 'funny_video_online_widget_url_title' ] = funny_video_online_READER_TITLE;
+		$newoptions[ 'funny_video_online_widget_RSS_url' ] = funny_video_online_READER_URL_RSS_DEFAULT;
+		$newoptions['funny_video_online_widget_RSS_count_items'] = funny_video_online_MAX_SHOWN_ITEMS;		
+	}
+	if ( $_POST["funny_video_online_widget-submit"] ) {
+		$newoptions['funny_video_online_widget_url_title'] = strip_tags(stripslashes($_POST["funny_video_online_widget_url_title"]));
+		$newoptions['funny_video_online_widget_RSS_url'] = funny_video_online_READER_URL_RSS_DEFAULT;
+		$newoptions['funny_video_online_widget_RSS_count_items'] = strip_tags(stripslashes($_POST["funny_video_online_widget_RSS_count_items"]));
+	}	
+		
+	if ( $options != $newoptions ) {
+		$options = $newoptions;
+		update_option('funny_video_online_widget', $options);		
+	}
+	$funny_video_online_widget_url_title = wp_specialchars($options['funny_video_online_widget_url_title']);
+	$funny_video_online_widget_RSS_url = funny_video_online_READER_URL_RSS_DEFAULT;	
+	$funny_video_online_widget_RSS_count_items = $options['funny_video_online_widget_RSS_count_items'];
+	
+	?><form method="post" action="">	
+
+	<p><label for="funny_video_online_widget_url_title"><?php _e('Title:'); ?> <input style="width: 350px;" id="funny_video_online_widget_url_title" name="funny_video_online_widget_url_title" type="text" value="<?php echo $funny_video_online_widget_url_title; ?>" /></label></p>
+ 
+	<p><label for="funny_video_online_widget_RSS_count_items"><?php _e('Count Items To Show:'); ?> <input  id="funny_video_online_widget_RSS_count_items" name="funny_video_online_widget_RSS_count_items" size="2" maxlength="2" type="text" value="<?php echo $funny_video_online_widget_RSS_count_items?>" /></label></p>
+	
+	<br clear='all'></p>
+	<input type="hidden" id="funny_video_online_widget-submit" name="funny_video_online_widget-submit" value="1" />	
+	</form>
+	<?php
+}
+
+add_action('admin_menu', 'funny_video_online_menu');
+
+function funny_video_online_menu() {
+	add_options_page('Funny video online', 'Funny video online', 8, __FILE__, 'funny_video_online_options');
+}
+
+function funny_video_online_options() {	
+	?>
+	<div class="wrap">
+
+		<h2>Funny video online</h2>
+<p><b>Plugin "Funny video online" displays Funny video on your blog. There are over 10,000 video clips.
+Add Funny YouTube videos to your sidebar on your blog using  a widget.</b> </p>
+<p> <h3>Add the widget "Funny video online"  to your sidebar from Appearance->Widgets and configure the widget options.</h3></p>
+ <hr /> <hr />
+   		<h2>Joke of the Day</h2>
+<p><b>Plugin "Joke of the Day" displays categorized jokes on your blog. There are over 40,000 jokes in 40 categories. Jokes are saved on our database, so you don't need to have space for all that information. </b> </p>
+ <h3>Get plugin <a target="_blank" href="http://wordpress.org/extend/plugins/joke-of-the-day/">Joke of the Day</h3></a>
+   <hr /> <hr />
+ <h2>Real Estate Finder</h2>
+<p><b>Plugin "Real Estate Finder" gives visitors the opportunity to use a large database of real estate.
+Real estate search for U.S., Canada, UK, Australia</b> </p>
+<h3>Get plugin <a target="_blank" href="http://wordpress.org/extend/plugins/real-estate-finder/">Real Estate Finder</h3></a>
+ <hr /> <hr />
+
+ <h2>Jobs Finder</h2>
+<p><b>Plugin "Jobs Finder" gives visitors the opportunity to more than 1 million offer of employment.
+Jobs search for U.S., Canada, UK, Australia</b> </p>
+<h3>Get plugin <a target="_blank" href="http://wordpress.org/extend/plugins/jobs-finder/">Jobs Finder</h3></a>
+ <hr /> <hr />
+		<h2>Recipe of the Day</h2>
+<p><b>Plugin "Recipe of the Day" displays categorized recipes on your blog. There are over 20,000 recipes in 40 categories. Recipes are saved on our database, so you don't need to have space for all that information.</b> </p>
+<h3>Get plugin <a target="_blank" href="http://wordpress.org/extend/plugins/recipe-of-the-day/">Recipe of the Day</h3></a>
+ <hr /> <hr />
+ <h2>WP Social Bookmarking</h2>
+<p><b>WP-Social-Bookmarking plugin will add a image below your posts, allowing your visitors to share your posts with their friends, on FaceBook, Twitter, Myspace, Friendfeed, Technorati, del.icio.us, Digg, Google, Yahoo Buzz, StumbleUpon.</b></p>
+<p><b>Plugin suport sharing your posts feed on <a href="http://www.onlinerel.com/">OnlineRel</a>. This helps to promote your blog and get more traffic.</b></p>
+<p>Advertise your real estate, cars, items... Buy, Sell, Rent. Free promote your site:
+<ul>
+	<li><a target="_blank" href="http://www.onlinerel.com/">OnlineRel</a></li>
+	<li><a target="_blank" href="http://www.easyfreeads.com/">Easy Free Ads</a></li>
+	<li><a target="_blank" href="http://www.worldestatesite.com/">World Estate Site</a></li>
+</ul>
+<h3>Get plugin <a target="_blank" href="http://wordpress.org/extend/plugins/wp-social-bookmarking/">WP Social Bookmarking</h3></a>
+</p>
+	</div>
+	<?php
+}
+
+function funny_video_online_widget_Init()
+{
+  register_sidebar_widget(__('Funny video online'), 'funny_video_online_widget_ShowRss');
+  register_widget_control(__('Funny video online'), 'funny_video_online_widget_Admin', 500, 250);
+}
+add_action("plugins_loaded", "funny_video_online_widget_Init");
+?>
---
+++ readme.txt
@@ -0,0 +1,38 @@
+=== Funny video online ===
+Contributors: allis741 
+Donate link: http://www.onlinerel.com/wordpress-plugins/
+Tags: widget, jokes, video, funny video,YouTube videos,YouTube, funny, funny foto, funny jokes, post, posts, link, links, promote, SEO, category, cloud, sidebar, feed, rss, popular
+Requires at least: 2.5
+Tested up to: 3.0.1
+Stable tag: 1.0
+
+Plugin "Funny video online" displays Funny video on your blog. There are over 10,000 video clips.
+Add Funny YouTube videos to your sidebar on your blog using  a widget.
+ 
+== Description ==
+
+Plugin "Funny video online" displays Funny video on your blog. There are over 10,000 video clips.
+Add Funny YouTube videos to your sidebar on your blog using  a widget. Video are saved on our database, so you don't need to have space for all that information. 
+
+<b>Try the Demo</b>
+
+If you're interested in seeing what a default installation of Funny video online has to offer, try out the <a target="_blank" href="http://fun.onlinerel.com/">Funny video online</a>
+
+== Installation ==
+
+1. Upload the folder funny-video-online to the /wp-content/plugins/ directory
+2. Activate the plugin Funny video online through the 'Plugins' menu in WordPress
+3. Add the widget to your sidebar from Appearance->Widgets and configure the widget options.
+
+== Screenshots ==
+
+1. Funny video online Widget
+
+== Frequently Asked Questions ==
+
+Please see <a href="http://www.onlinerel.com/wordpress-plugins/">FAQ</a>.
+
+== Changelog ==
+
+ = 1.0 =
+ First stable version.
